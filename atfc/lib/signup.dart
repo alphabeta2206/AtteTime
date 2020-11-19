29fccf83b0a9a2274f0a29ece703944bdb064705
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-11-19 17:06:45 +0530

    Login and signup pages added

---
+++ atfc/lib/signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  Register({Key key}) : super(key: key);
+
+  @override
+  _registerState createState() => _registerState();
+}
+
+class _registerState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
+    );
+  }
+}
