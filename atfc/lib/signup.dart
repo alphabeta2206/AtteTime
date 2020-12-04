d90ae3bbda36c20e8a8d80de4b105cdeda0629ac
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-12-04 08:13:36 +0530

    progress bar anf core dep added

--- atfc/lib/signup.dart
+++ atfc/lib/signup.dart
@@ -24,48 +24,50 @@ class _RegisterState extends State<Register> {
         title: Text("Atteface"),
         backgroundColor: Colors.green[400],
       ),
-      body: Padding(
-          padding: EdgeInsets.fromLTRB(15, 200, 15, 10),
-          child: Column(
-            children: [
-              TextField(
-                controller: _email,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Enter Email",
+      body: Container(
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(15, 200, 15, 10),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _email,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter Email",
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                  controller: _password,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Enter Password",
-                  )),
-              SizedBox(height: 10),
-              TextField(
-                  controller: _confirmpassword,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Confirm Password",
-                  )),
-              SizedBox(height: 10),
-              GestureDetector(
-                  onTap: () => {
-                    
-                  },
-                  child: Container(
-                      //height:30,
-                      //width:100,
-                      padding: EdgeInsets.fromLTRB(150, 15, 150, 15),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blue),
-                      child: Text("Register"))),
-            ],
-          )),
+                SizedBox(height: 10),
+                TextField(
+                    controller: _password,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Enter Password",
+                    )),
+                SizedBox(height: 10),
+                TextField(
+                    controller: _confirmpassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Confirm Password",
+                    )),
+                SizedBox(height: 10),
+                GestureDetector(
+                    onTap: ()  async{
+
+                    },
+                    child: Container(
+                        //height:30,
+                        //width:100,
+                        padding: EdgeInsets.fromLTRB(150, 15, 150, 15),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.blue),
+                        child: Text("Register"))),
+              ],
+            )),
+      ),
     );
   }
 }
