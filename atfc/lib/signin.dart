999dd78d98da03c627d15d1daa90db6b3ced90d0
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-12-04 09:09:23 +0530

    login firebase added

--- atfc/lib/signin.dart
+++ atfc/lib/signin.dart
@@ -2,7 +2,7 @@ import 'package:atfc/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 class Login extends StatefulWidget {
   Login({Key key}) : super(key: key);
 
@@ -12,12 +12,11 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final _auth = FirebaseAuth.instance;
+  bool showProgress = false;
   TextEditingController _email = TextEditingController();
   TextEditingController _password = TextEditingController();
 
-  void onLogin() {
-    
-  }
+  void onLogin() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,55 +24,82 @@ class _LoginState extends State<Login> {
           title: Text("Atteface"),
           backgroundColor: Colors.green[400],
         ),
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(15, 200, 15, 10),
-            child: Column(
-              children: [
-                TextField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Enter Email",
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                    controller: _password,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Enter Password",
-                    )),
-                SizedBox(height: 10),
-                GestureDetector(
-                    onTap: ()  async{
-                      
-                    },
-                    child: Container(
-                        //height:30,
-                        //width:100,
-                        padding: EdgeInsets.fromLTRB(150, 15, 150, 15),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.blue),
-                        child: Text("Login"))),
-                SizedBox(height: 10),
-                InkWell(
-                    onTap: () => {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Register()))
+        body: Center(
+          child: ModalProgressHUD(
+            inAsyncCall: showProgress,
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(15, 200, 15, 10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter Email",
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                        controller: _password,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Enter Password",
+                        )),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        onTap: () async {
+                          setState(() {
+                            showProgress = true;
+                          });
+                          try {
+                            final newUser = await _auth.signInWithEmailAndPassword(
+                              email: _email.text, password: _password.text);
+                              print(newUser.toString());
+
+                              if (newUser != null) {
+                        Fluttertoast.showToast(
+                            msg: "Login Successfull",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.blueAccent,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                        setState(() {
+                          showProgress = false;
+                        });
+                      }
+                          } 
+                          catch (e) {}
                         },
-                    child: Container(
-                        //height:30,
-                        //width:100,
-                        padding: EdgeInsets.fromLTRB(143, 15, 143, 15),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.blue),
-                        child: Text("Register")))
-              ],
-            )));
+                        child: Container(
+                            //height:30,
+                            //width:100,
+                            padding: EdgeInsets.fromLTRB(150, 15, 150, 15),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.blue),
+                            child: Text("Login"))),
+                    SizedBox(height: 10),
+                    InkWell(
+                        onTap: () => {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Register()))
+                            },
+                        child: Container(
+                            //height:30,
+                            //width:100,
+                            padding: EdgeInsets.fromLTRB(143, 15, 143, 15),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.blue),
+                            child: Text("Register")))
+                  ],
+                )),
+          ),
+        ));
   }
 }
