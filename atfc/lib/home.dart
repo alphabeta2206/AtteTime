e9360166f3bdedbb58e98c0bc9e7b6f622743ee3
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2021-02-02 15:34:18 +0530

    Sliding Tab added

--- atfc/lib/home.dart
+++ atfc/lib/home.dart
@@ -8,12 +8,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  int x;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Atteface"),
-        backgroundColor: Colors.green[400],
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 7,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Timetable'),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[0]),
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[1]),
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[2]),
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[3]),
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[4]),
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[5]),
+                Tab(icon: Icon(Icons.calendar_view_day), text: days[6]),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Container(child:Text(days[0])),
+              Container(child:Text(days[1])),
+              Container(child:Text(days[2])),
+              Container(child:Text(days[3])),
+              Container(child:Text(days[4])),
+              Container(child:Text(days[5])),
+              Container(child:Text(days[6])),
+            ],
+          ),
+        ),
       ),
     );
   }
